da85c9ea8eb39ecff1a548054a1efa54f9a3d6a4
Arash <arash.arghavan@gmail.com>
2019-04-09 22:21:55 -0500

    all done

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
 
-CREATE TABLE albums (
+CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
     name  VARCHAR(100) NOT NULL,
---
+++ select_exercises.sql
@@ -0,0 +1,32 @@
+
+
+USE codeup_test_db;
+
+-- SELECT 'Name of Albums by Pink Floyd' AS 'SELECT';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+-- SELECT 'Year of Sgt. Pepper''s Lonely Hearts Club Band' AS 'Year';
+SELECT release_year
+FROM albums
+WHERE name='Sgt. Pepper''s Lonely Hearts Club Band';
+
+-- SELECT 'Genre of Nevermind';
+SELECT gener
+FROM albums
+WHERE name = 'Nevermind';
+
+-- SELECT 'Albums from the 90''s';
+SELECT *
+FROM albums
+WHERE realese_date BETWEEN 1990 AND 1999;
+
+-- SELECT 'Albums that sold less than 20 million';
+SELECT *
+FROM albums
+WHERE sales < 20;
+
+--SELECT 'All the albums with a genre of Rock';
+SELECT *
+FROM albums
+WHERE gener = 'Rock';
+
